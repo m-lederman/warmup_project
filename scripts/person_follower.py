04a342b84814237d57b25da71b986594f4f0479f
m-lederman <mlederman@uchicago.edu>
2022-04-11 13:37:34 +0000

    add

---
+++ scripts/person_follower.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+
+# TOPICS:
+#   cmd_vel: publish to, used for setting robot velocity
+#   scan   : subscribing, where the wall is
+
+import rospy
+
+# msg needed for /scan.
+from sensor_msgs.msg import LaserScan
+
+# msgs needed for /cmd_vel.
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+
+# How close we will get to the person.
+min_dis = 0.5
+
+max_dis = 2.0
+
+class PersonFollower(object):
+    """ This node walks the robot to wall and stops """
+
+    def __init__(self):
+        # Start rospy node.
+        rospy.init_node("follow_person")
+
+        # Declare our node as a subscriber to the scan topic and
+        #   set self.process_scan as the function to be used for callback.
+        rospy.Subscriber("/scan", LaserScan, self.process_scan)
+
+        # Get a publisher to the cmd_vel topic.
+        self.twist_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+
+        # Create a default twist msg (all values 0).
+        lin = Vector3()
+        ang = Vector3()
+        self.twist = Twist(linear=lin,angular=ang)
+
+    def process_scan(self, data):
+        # Determine closeness to wall by looking at scan data from in front of
+        #   the robot, set linear velocity based on that information, and
+        #   publish to cmd_vel.
+
+        # The ranges field is a list of 360 number where each number
+        #   corresponds to the distance to the closest obstacle from the
+        #   LiDAR at various angles. Each measurement is 1 degree apart.
+
+        # The first entry in the ranges list corresponds with what's directly
+        #   in front of the robot.
+        lvel = 0
+        rvel = 0
+        right = 0
+        left = 0
+        
+        for i in range (179):
+            right = data.ranges[-i]
+            print(right)
+            left = data.ranges[i]
+            print(left)
+            if (right == 0 or left == 0 or right > max_dis or left > max_dis):
+                self.twist.angular.z = 0
+                
+            if (right > 0 and right <= max_dis and (left == 0 or left > max_dis)):
+                self.twist.angular.z = -i/5
+                print("r")
+                print(i)
+                
+                break
+            
+            if (left > 0 and left <= max_dis and (right == 0 or right > max_dis)):
+                self.twist.angular.z = i/5
+                print("l")
+                print(i)
+              ##  print(2)
+                break
+            if (data.ranges[180] > 0 and data.ranges[180] <= max_dis):
+                self.twist.angular.z = 30
+            ##print(self.twist.angular.z)
+            
+        for i in range(11):
+            
+            lvel = data.ranges[i]
+            
+            rvel = data.ranges[-i]
+           
+            if ((lvel >= min_dis and lvel < max_dis) or (rvel >= min_dis and rvel < max_dis)):
+                # Go forward if not close enough to person.
+                self.twist.linear.x = lvel / 2 + rvel /2
+                break
+            if ((lvel == 0.0 or lvel >= max_dis or lvel <= min_dis) and (rvel == 0.0 or rvel >= 3.1 or rvel <= min_dis)) :
+                ##self.twist.angular.z = (right + left) * 0.5
+                self.twist.linear.x = 0
+        
+        ##print(self.twist.linear.x)
+        # Publish msg to cmd_vel.
+        ##rospy.sleep(100)
+        self.twist_pub.publish(self.twist)
+        
+
+
+    def run(self):
+        # Keep the program alive.
+        rospy.spin()
+
+if __name__ == '__main__':
+    # Declare a node and run it.
+    node = PersonFollower()
+    node.run()
---
+++ scripts/wall_follower.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+
+
+# TOPICS:
+#   cmd_vel: publish to, used for setting robot velocity
+#   scan   : subscribing, where the wall is
+
+import rospy
+
+# msg needed for /scan.
+from sensor_msgs.msg import LaserScan
+
+# msgs needed for /cmd_vel.
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+
+# How close we will get to the person.
+min_dis = 0.1
+
+
+
+class WallFollower(object):
+    """ This node walks the robot to wall and stops """
+
+    def __init__(self):
+        # Start rospy node.
+        rospy.init_node("follow_person")
+        
+        # Declare our node as a subscriber to the scan topic and
+        #   set self.process_scan as the function to be used for callback.
+        rospy.Subscriber("/scan", LaserScan, self.process_scan)
+
+        # Get a publisher to the cmd_vel topic.
+        self.twist_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+
+        # Create a default twist msg (all values 0).
+        lin = Vector3()
+        ang = Vector3()
+        self.twist = Twist(linear=lin,angular=ang)
+        self.twist_pub.publish(self.twist)
+        
+
+    def process_scan(self, data):
+        # Determine closeness to wall by looking at scan data from in front of
+        #   the robot, set linear velocity based on that information, and
+        #   publish to cmd_vel.
+
+        # The ranges field is a list of 360 number where each number
+        #   corresponds to the distance to the closest obstacle from the
+        #   LiDAR at various angles. Each measurement is 1 degree apart.
+
+        # The first entry in the ranges list corresponds with what's directly
+        #   in front of the robot.
+        vel = data.ranges[90]
+        left = 0
+        turn = 0.25
+        iturn = -50
+        safety = 1
+        isafety = -50
+        
+
+       ## print(vel)
+        
+        for i in range (181):
+            left = data.ranges[i]
+            if (left < turn and left >= min_dis):
+                turn = left 
+                iturn = i
+                
+            if (left < safety and left >= min_dis):
+                safety = left 
+                isafety = i
+
+        print(iturn)
+
+        self.twist.angular.z = (iturn - 90) / 100
+
+        self.twist.angular.z = (iturn - 90) / 100
+        self.twist.linear.x = 0
+        
+        self.twist.linear.x = 0.2/ (1+ abs(90-iturn))
+        if (iturn == -50):
+            self.twist.angular.z = 0
+            self.twist.linear.x = 0.1
+            if (isafety >= 30):
+                self.twist.angular.z = 1
+                self.twist.linear.x = 0.1
+            
+        
+        print(self.twist.linear.x)
+        # Publish msg to cmd_vel.
+        ##rospy.sleep(100)
+        self.twist_pub.publish(self.twist)
+        
+
+
+    def run(self):
+        # Keep the program alive.
+        rospy.spin()
+
+if __name__ == '__main__':
+    # Declare a node and run it.
+    node = WallFollower()
+    node.run()
